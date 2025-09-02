4bd224cad72dcc6090d14fcf0a3f994b6dffba85
lari timoteo <larissatimoteo.678@gmail.com>
2025-09-02 08:01:15 -0300

    Inclui exemplo e ícone no tabNavigator

--- Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
+++ Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { RootStackParamList, TabParamList } from "./types";
 
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 // Telas do app.
 import HomeScreen from "../screens/HomeScreen";
 // importar depois que implementar: DetailsScreen, SettingsScreen
@@ -12,9 +13,26 @@ import LoginScreen from "../screens/LoginScreen";
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
 
+
 function TabNavigator() {
-    return (
-      <Tab.Navigator>
+  return (
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => ({
+        tabBarIcon: ({ color, focused, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home";
+          }
+
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+      })}
+    >
+
       <Tab.Screen name="Home" component={HomeScreen}/>
       <Tab.Screen name="Settings" component={HomeScreen} />
       <Tab.Screen name="Register" component={RegisterScreen} />
--- Etapa3/loja-react-app/src/screens/LoginScreen.tsx
+++ Etapa3/loja-react-app/src/screens/LoginScreen.tsx
@@ -1,8 +1,11 @@
 import React, { use, useState } from "react";
-import { View, TextInput, Button, StyleSheet, Text } from "react-native";
+import { View, TextInput, Button, StyleSheet, Text, SafeAreaView} from "react-native";
 
 import { fakeLogin } from "../services/authService";
 import { useAuth } from "../contexts/AuthContext";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+
 
 export default function LoginScreen({ navigation }: any) {
   const [email, setEmail] = useState('');
@@ -22,7 +25,8 @@ const handleLogin = async () => {
   }
   
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View>
       <Text>Email:</Text>
       <TextInput
         style={styles.input}
@@ -51,8 +55,8 @@ const handleLogin = async () => {
         <Button title="Registrar"
         onPress={() => navigation.
         navigate('Register')} />
-
-    </View>
+       </View>
+    </SafeAreaView>
     
   );
 }  
