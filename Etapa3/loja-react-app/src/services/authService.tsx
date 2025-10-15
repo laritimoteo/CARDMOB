8f943b006bb8e7baabd89fea93b383e43362047b
lari timoteo <larissatimoteo.678@gmail.com>
2025-10-15 10:00:51 -0300

    Tela de cadastro

--- Etapa3/loja-react-app/src/services/authService.tsx
+++ Etapa3/loja-react-app/src/services/authService.tsx
@@ -2,38 +2,60 @@ import Constants from 'expo-constants';
 
 // const API_URL = Config.API_URL;
 // const API_URL = 'http://10.81.205.23:5000';
-const { apiUrl } = Constants.expoConfig?.extra || {};
 
+
+const { apiUrl } = Constants.expoConfig?.extra || {}; // novo
 
 export async function fakeLogin(email: string, password: string): Promise<string> {
-  if (email === 'teste@example.com' && password === '123') {
-      return Promise.resolve('fake-jwt-token');
-  }
-  return Promise.reject('Credenciais inválidas');
+    if (email === 'teste@example.com' && password === '123') {
+        return Promise.resolve('fake-jwt-token');
+    }
+    return Promise.reject('Credenciais inválidas');
 }
 
+
 export async function requestLogin(email: string, password: string): Promise<string> {
-  console.log(apiUrl);
-  if (email === 'admin@example.com' && password === 'admin123') {
-      try {
-          const response = await fetch(`${apiUrl}/api/users/login`, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({email, password}),
-          });
-          const data = await response.json();
-          const jwt = data.accessToken;
-          console.log(jwt);
-          return Promise.resolve(jwt);
-      }
-      catch (error) {
-          console.error(error);
-          return Promise.reject(error);
-      }
-  }
+    console.log(apiUrl); // alterado
+    try {
+        // alterado
+        const response = await fetch(`${apiUrl}/api/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+        });
+        const data = await response.json();
+        const jwt = data.accessToken;
+        console.log(jwt);
+        return Promise.resolve(jwt);
+    }
+    catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+        // return Promise.reject('Credenciais inválidas');
+    }
+}
 
 
-  return Promise.reject('Credenciais inválidas');
+export async function requestRegister(name: string,email: string, password: string): Promise<string> {
+    try {
+        // alterado
+        const response = await fetch(`${apiUrl}/api/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name ,email, password}),
+        });
+        const data = await response.json();
+        const jwt = data.accessToken;
+        console.log(jwt);
+        return Promise.resolve(jwt);
+    }
+    catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+        // return Promise.reject('Credenciais inválidas');
+    }
 }
